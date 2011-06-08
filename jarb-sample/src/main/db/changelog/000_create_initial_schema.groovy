f8eadef3dc3f741a1d70ca2034eada836fc6f294
Jeroen van Schagen <jeroen@it-techniek.nl>
2011-06-08 10:53:50 +0200

    sample app now starts up

--- jarb-sample/src/main/db/changelog/000_create_initial_schema.groovy
+++
@@ -1,5 +0,0 @@
-databaseChangeLog() {
-	changeSet(author: "jeroen@42.nl", id: "1") {
-		comment("Create initial database schema.")
-	}
-}
---
+++ jarb-sample/src/main/db/changelogs/000_create_initial_schema.groovy
@@ -0,0 +1,14 @@
+databaseChangeLog() {
+	changeSet(author: "jeroen@42.nl", id: "1") {
+		comment("Create initial database schema.")
+		createTable(tableName: "posts") {
+            column(autoIncrement: true, name: "id", type: "BIGINT") {
+                constraints(nullable: false, primaryKey: true, primaryKeyName: "pk_posts_id")
+            }
+            column(name: "posted_datetime", type: "DATETIME") { constraints(nullable: false) }
+            column(name: "author", type: "VARCHAR(255)") { constraints(nullable: false) }
+            column(name: "title", type: "VARCHAR(255)") { constraints(nullable: false) }
+            column(name: "message", type: "TEXT")
+        }
+	}
+}
