cd1cc4d3b5f9747358e18dcad2646267de4a5136
Jeroen van Schagen <jeroen@it-techniek.nl>
2011-06-10 14:33:20 +0200

    added javadoc to js

--- jarb-sample/src/main/webapp/scripts/posts_structure.js
+++ jarb-sample/src/main/webapp/scripts/posts_structure.js
@@ -1,13 +1,24 @@
-$.getJSON('posts/structure.json', function(json) {
+/**
+ * Retrieve constraint information about posts and applies
+ * that information to the creation form.
+ * @author Jeroen van Schagen
+ */
 
-	$.each(json.beanConstraintMetadata.propertiesMetadata, function(index, propertyMetadata) {
-		var inputField = $('input[name=' + propertyMetadata.propertyName + ']');
+$.getJSON('posts/structure.json', function(data) {
+
+	$.each(data.beanConstraintMetadata.propertiesMetadata, function(index, propertyMetadata) {
+		var inputField = $('input[name=' + propertyMetadata.name + ']');
 		inputField.attr('minlength', propertyMetadata.minimumLength);
 		inputField.attr('length', propertyMetadata.maximumLength);
 		if(propertyMetadata.required) {
-			$('label[for=' + propertyMetadata.propertyName + "]").append(" (*)");
+			$('label[for=' + propertyMetadata.name + "]").append(" (*)");
 			inputField.addClass('required');
 		}
+		$.each(propertyMetadata.types, function(index, propertyType) {
+			if(propertyType == 'EMAIL') {
+				inputField.addClass('email');
+			}
+		});
 	});
 
 });
--- jarb-sample/src/main/webapp/scripts/posts_submit.js
+++ jarb-sample/src/main/webapp/scripts/posts_submit.js
@@ -1,6 +1,13 @@
+/**
+ * Intercepts create post submits to perform client side validation
+ * and add newly created posts to the index without page refreshing.
+ * 
+ * @author Jeroen van Schagen
+ */
+
 $(document).ready(function() {
     var createForm = $('.create form');
-    createForm.validate();
+    createForm.validate(); // Init validator
     
     createForm.submit(function(e) {
     	// Perform validation checks before posting
@@ -14,6 +21,7 @@ $(document).ready(function() {
 					$('.status p').text(data.status);
 					if(data.success) {
 						// New post has been accepted accepted
+						// Append post to the index, meaning we dont have to refresh
 						var newPost = '<li>';
 						newPost += '<h2>' + data.post.title + '</h2>';
 						newPost += '<span class="author">' + data.post.author + '</span>'
@@ -22,6 +30,7 @@ $(document).ready(function() {
 						newPost += '</li>'
 						$('.posts ul').append(newPost);
 						$('.posts li:last').fadeIn();
+						// Clear creation form
 						$(':input', '.create')
 							.not(':button, :submit, :reset, :hidden')
 							.removeAttr('checked')
