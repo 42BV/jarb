2f465503acf692d8592d51195995385072cf0b25
Jeroen van Schagen <jeroen.v.schagen@gmail.com>
2011-06-20 17:13:49 +0200

    database popoulator executor now also accepts a collection of populators during construction

--- jarb-populator/src/main/java/org/jarb/populator/DatabasePopulatorExecutor.java
+++ jarb-populator/src/main/java/org/jarb/populator/DatabasePopulatorExecutor.java
@@ -1,5 +1,7 @@
 package org.jarb.populator;
 
+import java.util.Collection;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.util.Assert;
@@ -17,12 +19,20 @@ public final class DatabasePopulatorExecutor {
     private final DatabasePopulator populator;
     
     /**
-     * Construct a new {@link DatabasePopulatorExecutor}.
+     * Construct a new {@link DatabasePopulatorExecutor} that executes one populator.
      * @param populator database populator being invoked
      */
     public DatabasePopulatorExecutor(DatabasePopulator populator) {
         Assert.notNull(populator, "Database populator cannot be null");
         this.populator = populator;
+    }
+    
+    /**
+     * Construct a new {@link DatabasePopulatorExecutor} that executes a collection of populators.
+     * @param populators database popoulators being invoked
+     */
+    public DatabasePopulatorExecutor(Collection<DatabasePopulator> populators) {
+        this(new CompoundDatabasePopulator(populators));
     }
     
     /**
