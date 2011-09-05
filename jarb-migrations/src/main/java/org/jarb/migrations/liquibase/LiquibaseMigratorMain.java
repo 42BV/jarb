ba698651c273474c915f99c3e9964355e00dbdc9
Jeroen van Schagen <jeroen@42.nl>
2011-09-05 13:58:47 +0200

    migrator main code cleaned up, can now also specify a basePath and dropFirst arg

--- jarb-migrations/src/main/java/org/jarb/migrations/liquibase/LiquibaseMigratorMain.java
+++ jarb-migrations/src/main/java/org/jarb/migrations/liquibase/LiquibaseMigratorMain.java
@@ -1,41 +1,129 @@
 package org.jarb.migrations.liquibase;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import liquibase.resource.FileSystemResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 
 import org.jarb.migrations.DatabaseMigrator;
 import org.jarb.utils.JdbcUtils;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.Parameters;
 
 /**
  * This class provides command-line support for Liquibase migrations.
  * 
- * @author BVos
+ * @author Bas de Vos
+ * @author Jeroen van Schagen
+ * 
+ * @since 5 September 2011
  */
-public class LiquibaseMigratorMain {
+public final class LiquibaseMigratorMain {
 
     /**
      * Invokes the Liquibase migrations provided.
      * @param args all migration arguments
      */
-    public static void main(String[] args) throws SQLException {
-        LiquibaseMigratorMainArgs params = new LiquibaseMigratorMainArgs();
-        JCommander cmd = new JCommander(params);
-        Connection conn = null;
+    public static void main(String[] args) {
+        LiquibaseMigrationCommand migrationCommand = new LiquibaseMigrationCommand();
+        JCommander argumentParser = new JCommander(migrationCommand);
         try {
-            cmd.parse(args);
-            conn = params.createConnection();
-            DatabaseMigrator migrator = params.createMigrator();
-            migrator.migrate(conn);
-            conn.commit();
+            argumentParser.parse(args);
+            migrationCommand.execute();
         } catch (ParameterException pe) {
             pe.printStackTrace();
-            cmd.usage();
-        } finally {
-            JdbcUtils.closeQuietly(conn);
+            argumentParser.usage();
         }
+    }
+
+    @Parameters(separators = "= ")
+    public static final class LiquibaseMigrationCommand {
+
+        @Parameter(names = "-driverClass", required = true, description = "Fully qualified name of the JDBC driver class")
+        private String driverClassName;
+
+        @Parameter(names = "-dbUrl", required = true, description = "Database URL")
+        private String dbUrl;
+
+        @Parameter(names = "-dbUser", required = true, description = "Database username")
+        private String dbUserName;
+
+        @Parameter(names = "-dbPassword", required = true, description = "Database password")
+        private String dbPassword;
+
+        @Parameter(names = "-changeLogBaseDir", description = "Liquibase changelog base dir (optional)")
+        private String changeLogBaseDir;
+
+        @Parameter(names = "-changeLogPath", required = true, description = "Liquibase changelog path")
+        private String changeLogPath;
+
+        @Parameter(names = "-sqlOutputPath", description = "SQL output file path (optional)")
+        private String sqlOutputPath;
+
+        @Parameter(names = "-dropFirst", description = "Drop first (optional)")
+        private boolean dropFirst = false;
+
+        /**
+         * Perform Liquibase migration on the database.
+         */
+        public void execute() {
+            Connection connection = null;
+            try {
+                connection = createConnection();
+                createMigrator().migrate(connection);
+                connection.commit();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } finally {
+                JdbcUtils.closeQuietly(connection);
+            }
+        }
+
+        /**
+         * Factory method that creates a {@link DatabaseMigrator} based upon given arguments.
+         * @return new DatabaseMigrator, capable of performing database migrations
+         */
+        private DatabaseMigrator createMigrator() {
+            LiquibaseMigrator migrator = new LiquibaseMigrator();
+            migrator.setChangeLogPath(changeLogPath);
+            migrator.setResourceAccessor(createResourceAccessor());
+            migrator.setOutputFilePath(sqlOutputPath);
+            migrator.setDropFirst(dropFirst);
+            return migrator;
+        }
+
+        private ResourceAccessor createResourceAccessor() {
+            FileSystemResourceAccessor accessor;
+            if (isBlank(changeLogBaseDir)) {
+                accessor = new FileSystemResourceAccessor();
+            } else {
+                accessor = new FileSystemResourceAccessor(changeLogBaseDir);
+            }
+            return accessor;
+        }
+
+        /**
+         * Factory method that creates a JDBC connection based upon given arguments.
+         * @return connection to the database
+         */
+        private Connection createConnection() {
+            try {
+                Class.forName(driverClassName); // Register appropriate JDBC driver
+                return DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+            } catch (SQLException sqle) {
+                throw new RuntimeException(sqle);
+            } catch (ClassNotFoundException cnfe) {
+                throw new RuntimeException(cnfe);
+            }
+        }
+
     }
 
 }
--- jarb-migrations/src/test/java/org/jarb/migrations/liquibase/LiquibaseMigratorMainTest.java
+++ jarb-migrations/src/test/java/org/jarb/migrations/liquibase/LiquibaseMigratorMainTest.java
@@ -4,68 +4,78 @@ import static junit.framework.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.sql.SQLException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-/**
- * Tests for the LiquibaseMigratorMain class.
- * 
- * @author BVos
- */
 public class LiquibaseMigratorMainTest {
 
+    /**
+     * Perform a successful migration.
+     */
     @Test
-    public void testDbMigration() throws SQLException {
-        String[] migrationArguments = { "-changelogPath", "src/test/resources/changelog.groovy", "-dbPassword", "", "-dbUrl",
-                "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser", "sa", "-driverClass", "org.hsqldb.jdbcDriver" };
-        LiquibaseMigratorMain.main(migrationArguments);
+    public void testDbMigration() {
+        String[] args = { "-changeLogPath", "src/test/resources/changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser",
+                "sa", "-driverClass", "org.hsqldb.jdbcDriver", "-dropFirst" };
+        LiquibaseMigratorMain.main(args);
     }
 
+    /**
+     * Use a base directory and relative file name.
+     */
     @Test
-    public void testNoArgs() throws SQLException {
-        ByteArrayOutputStream baosOut = new ByteArrayOutputStream();
-        ByteArrayOutputStream baosErr = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(baosOut));
-        System.setErr(new PrintStream(baosErr));
+    public void testDbMigrationRelativePath() {
+        String[] args = { "-changeLogBaseDir", "src/test/resources", "-changeLogPath", "changelog.groovy", "-dbPassword", "", "-dbUrl",
+                "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser", "sa", "-driverClass", "org.hsqldb.jdbcDriver", "-dropFirst" };
+        LiquibaseMigratorMain.main(args);
+    }
+
+    /**
+     * Specified driver class has to be known by the class loader.
+     */
+    @Test(expected = RuntimeException.class)
+    public void testInvalidDriverClass() {
+        String[] args = { "-changeLogPath", "src/main/db/changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser", "sa",
+                "-driverClass", "org.hsqldb.invalidDriver" };
+        LiquibaseMigratorMain.main(args);
+    }
+
+    /**
+     * Database user should be valid.
+     */
+    @Test(expected = RuntimeException.class)
+    public void testConnectionCouldNotBeOpened() {
+        String[] args = { "-changeLogPath", "src/main/db/changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser",
+                "unknown", "-driverClass", "org.hsqldb.jdbcDriver" };
+        LiquibaseMigratorMain.main(args);
+    }
+
+    /**
+     * Specified change log should exist.
+     */
+    @Test(expected = RuntimeException.class)
+    public void testChangeLogDoesNotExist() {
+        String[] args = { "-changeLogPath", "src/test/resources/failing-changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat",
+                "-dbUser", "sa", "-driverClass", "org.hsqldb.jdbcDriver" };
+        LiquibaseMigratorMain.main(args);
+    }
+
+    /**
+     * Whenever invalid arguments are provided, show the usage.
+     */
+    @Test
+    public void testInvalidUsage() {
+        ByteArrayOutputStream tmpOut = new ByteArrayOutputStream();
+        ByteArrayOutputStream tmpErr = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(tmpOut));
+        System.setErr(new PrintStream(tmpErr));
         try {
             LiquibaseMigratorMain.main(new String[] { "" });
-            assertTrue(baosOut.toString().startsWith("Usage"));
-            assertTrue(baosErr.toString().indexOf("Exception") > 0);
+            assertTrue(tmpOut.toString().startsWith("Usage"));
+            assertTrue(tmpErr.toString().indexOf("Exception") > 0);
         } finally {
             System.setOut(System.out);
             System.setErr(System.err);
         }
     }
 
-    @Test(expected = RuntimeException.class)
-    public void testInvalidDriverClass() throws SQLException {
-        String[] args = { "-changelogPath", "src/main/db/changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser", "sa",
-                "-driverClass", "org.hsqldb.invalidDriver" };
-        LiquibaseMigratorMain.main(args);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void testInvalidDbUser() throws SQLException {
-        String[] args = { "-changelogPath", "src/main/db/changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat", "-dbUser",
-                "unknown", "-driverClass", "org.hsqldb.jdbcDriver" };
-        LiquibaseMigratorMain.main(args);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void testInvalidDbScript() throws SQLException {
-        String[] args = { "-changelogPath", "src/test/resources/failing-changelog.groovy", "-dbPassword", "", "-dbUrl", "jdbc:hsqldb:mem:com.nidera.pcat",
-                "-dbUser", "sa", "-driverClass", "org.hsqldb.jdbcDriver" };
-        LiquibaseMigratorMain.main(args);
-    }
-
-    @Test
-    @Ignore("Only for manual use; needs the oracle sandbox db")
-    public void testDbMigrationOracle() throws SQLException {
-        String[] args = { "-changelogPath", "src/main/db/changelog.groovy", "-dbPassword", "pcat3", "-dbUrl",
-                "jdbc:oracle:thin:@nidrtd57.nidera.nl:1521:s01ddt", "-dbUser", "s01_pcat3", "-driverClass", "oracle.jdbc.OracleDriver", "-sqlLogPath",
-                "c:/logging.txt" };
-        LiquibaseMigratorMain.main(args);
-    }
 }
