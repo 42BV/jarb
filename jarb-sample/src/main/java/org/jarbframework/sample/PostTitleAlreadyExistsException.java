e45a0543e0bed6549a1cea446d0ff06d09bfb094
Jeroen van Schagen <jeroen.v.schagen@gmail.com>
2011-09-26 18:17:52 +0200

    simplified sample project, now using spring data jpa

---
+++ jarb-sample/src/main/java/org/jarbframework/sample/PostTitleAlreadyExistsException.java
@@ -0,0 +1,22 @@
+package org.jarbframework.sample;
+
+import org.jarbframework.violation.DatabaseConstraintViolation;
+import org.jarbframework.violation.UniqueKeyViolationException;
+
+/**
+ * Thrown whenever a post is being inserted with a title that already exists.
+ * @author Jeroen van Schagen
+ * @since 8-6-2011
+ */
+public class PostTitleAlreadyExistsException extends UniqueKeyViolationException {
+    private static final long serialVersionUID = -7112473482705797922L;
+
+    /**
+     * Construct a new {@link PostTitleAlreadyExistsException}.
+     * @param violation constraint violation reference
+     * @param cause the actual cause
+     */
+    public PostTitleAlreadyExistsException(DatabaseConstraintViolation violation, Throwable cause) {
+        super(violation, cause);
+    }
+}
