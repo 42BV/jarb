b7451335b01affed2b92c456be958128a664313f
Sander Benschop <sander.benschop@42.nl>
2011-10-28 13:30:25 +0200

    Created testContains unittest for MetaModel class

--- jarb-populator-excel/src/test/java/org/jarbframework/populator/excel/metamodel/MetaModelTest.java
+++ jarb-populator-excel/src/test/java/org/jarbframework/populator/excel/metamodel/MetaModelTest.java
@@ -1,20 +1,21 @@
 package org.jarbframework.populator.excel.metamodel;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
-import org.jarbframework.populator.excel.metamodel.EntityDefinition;
-import org.jarbframework.populator.excel.metamodel.MetaModel;
 import org.junit.Before;
 import org.junit.Test;
 
+import domain.entities.Customer;
 import domain.entities.Workspace;
 
 public class MetaModelTest {
     private EntityDefinition<Workspace> workspaceDefinition;
     private MetaModel metamodel;
-    
+
     @Before
     public void setUp() {
         workspaceDefinition = EntityDefinition.forClass(Workspace.class).setTableName("workspaces").build();
@@ -30,5 +31,15 @@ public class MetaModelTest {
     public void testToString() {
         assertEquals("[class domain.entities.Workspace]", metamodel.toString());
     }
-    
+
+    @Test
+    public void testContains() {
+        assertTrue(metamodel.contains(domain.entities.Workspace.class));
+        assertFalse(metamodel.contains(domain.entities.Project.class));
+
+        EntityDefinition<Customer> customerDefinition = EntityDefinition.forClass(Customer.class).setTableName("customers").build();
+        metamodel = new MetaModel(Arrays.<EntityDefinition<?>> asList(customerDefinition));
+        assertTrue(metamodel.contains(domain.entities.SpecialCustomer.class));
+    }
+
 }
