8376ed157c4ed9174cda79013abea9b2ab72bca7
Jeroen van Schagen <jeroen.v.schagen@gmail.com>
2013-04-04 10:11:14 +0200

    Started on H2 violation resolver. Created a template for resolver integration testing.

--- jarb-constraints/src/main/java/org/jarbframework/constraint/violation/resolver/vendor/H2ViolationResolver.java
+++ jarb-constraints/src/main/java/org/jarbframework/constraint/violation/resolver/vendor/H2ViolationResolver.java
@@ -11,9 +11,23 @@ import org.jarbframework.constraint.violation.resolver.ViolationMessageResolver;
  */
 public class H2ViolationResolver implements ViolationMessageResolver {
 
+    /*
+        Data conversion error converting "'not a boolean' (CARS: ACTIVE BOOLEAN)"; SQL statement:
+        insert into cars (id, active, license_number, price) values (default, ?, ?, ?) -- (, ?1, ?2, ?3) [22018-171]
+        
+        Value too long for column "LICENSE_NUMBER VARCHAR(6) NOT NULL": "'1234567' (7)"; SQL statement:
+        insert into cars (id, active, license_number, price) values (default, ?, ?, ?) [22001-171]
+        
+        Unique index or primary key violation: "UK_CARS_LICENSE_NUMBER_INDEX_1 ON PUBLIC.CARS(LICENSE_NUMBER)"; SQL statement:
+        insert into cars (id, active, license_number, price) values (default, ?, ?, ?) [23505-171]
+        
+        NULL not allowed for column "LICENSE_NUMBER"; SQL statement:
+        insert into cars (id, active, license_number, price) values (default, ?, ?, ?) [23502-171]
+     */
+    
     @Override
     public DatabaseConstraintViolation resolveByMessage(String message) {
-        System.out.println(message);
+        // TODO: Create implementation
         return null;
     }
 
