25165ca5b99b74a1177cf2b8aa835caa31a6e4f1
Jeroen van Schagen <jeroen.v.schagen@gmail.com>
2013-04-29 10:16:23 +0200

    migration name space simplified further, can now also just provide a 'path'

--- jarb-migrations/src/main/java/org/jarbframework/migrations/MigrationsNamespaceHandler.java
+++ jarb-migrations/src/main/java/org/jarbframework/migrations/MigrationsNamespaceHandler.java
@@ -1,5 +1,6 @@
 package org.jarbframework.migrations;
 
+import org.jarbframework.migrations.liquibase.LiquibaseMigrator;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -19,18 +20,36 @@ public class MigrationsNamespaceHandler extends NamespaceHandlerSupport {
 
         @Override
         public BeanDefinition parse(Element element, ParserContext parserContext) {
-            final BeanDefinitionRegistry beanRegistry = parserContext.getRegistry();
-
-            String dataSourceId = element.getAttribute("data-source");
-            BeanDefinition dataSourceDefinition = beanRegistry.getBeanDefinition(dataSourceId);
+            final String dataSourceId = element.getAttribute("data-source");
+            BeanDefinitionRegistry beanRegistry = parserContext.getRegistry();
             
-            BeanDefinitionBuilder migrationDataSourceBuilder = BeanDefinitionBuilder.genericBeanDefinition(MigratingDataSource.class);
-            migrationDataSourceBuilder.addPropertyValue("delegate", dataSourceDefinition);
-            migrationDataSourceBuilder.addPropertyReference("migrator", element.getAttribute("migrator"));
-            
-            // Overwrite the bean definition with our migrating data source
-            beanRegistry.registerBeanDefinition(dataSourceId, migrationDataSourceBuilder.getBeanDefinition());
+            // Overwrite our existing data source definition with the migrating data source
+            BeanDefinition migratingDataSource = buildMigratingDataSource(element, dataSourceId, beanRegistry);
+            beanRegistry.registerBeanDefinition(dataSourceId, migratingDataSource);
             return null;
+        }
+
+        private BeanDefinition buildMigratingDataSource(Element element, String dataSourceId, BeanDefinitionRegistry beanRegistry) {
+            BeanDefinitionBuilder migratingDataSourceBuilder = BeanDefinitionBuilder.genericBeanDefinition(MigratingDataSource.class);
+            migratingDataSourceBuilder.addPropertyValue("delegate", beanRegistry.getBeanDefinition(dataSourceId));
+            migratingDataSourceBuilder.addPropertyValue("username", element.getAttribute("username"));
+            migratingDataSourceBuilder.addPropertyValue("password", element.getAttribute("password"));
+            addMigratorProperty(element, migratingDataSourceBuilder);
+            return migratingDataSourceBuilder.getBeanDefinition();
+        }
+
+        private void addMigratorProperty(Element element, BeanDefinitionBuilder dataSourceBuilder) {
+            if (element.hasAttribute("migrator")) {
+                dataSourceBuilder.addPropertyReference("migrator", element.getAttribute("migrator"));
+            } else {
+                dataSourceBuilder.addPropertyValue("migrator", buildLiquibaseMigrator(element));
+            }
+        }
+
+        private BeanDefinition buildLiquibaseMigrator(Element element) {
+            BeanDefinitionBuilder liquibaseMigratorBuilder = BeanDefinitionBuilder.genericBeanDefinition(LiquibaseMigrator.class);
+            liquibaseMigratorBuilder.addPropertyValue("changeLogPath", element.getAttribute("path"));
+            return liquibaseMigratorBuilder.getBeanDefinition();
         }
 
     }
