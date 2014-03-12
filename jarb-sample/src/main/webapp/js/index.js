383239c102dcb2bd0f19dacbd053e40c22165720
Jeroen van Schagen <jeroen.van.schagen@gmail.com>
2014-03-12 11:33:25 +0100

    Fixed messages in sample

--- jarb-sample/src/main/webapp/js/index.js
+++ jarb-sample/src/main/webapp/js/index.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	
+
 	$('#notes li').click(function() {
 		$(this).remove();
 	});
-	
-    $.ajax('/posts', {
-    	type: 'GET',
-    	success: function(data) {
-    		$('.posts tbody').empty();
-    		$.each(data, function(index, post) {
-    			appendPost(post);
-    		});
-    	}
-    });
-    
-    var appendPost = function(post) {
+
+	$.ajax('/posts', {
+	    type : 'GET',
+	    success : function(data) {
+		    $('.posts tbody').empty();
+		    $.each(data, function(index, post) {
+			    appendPost(post);
+		    });
+	    }
+	});
+
+	var appendPost = function(post) {
 		var newPost = '<tr>';
 		newPost += '<td>' + post.title + '</td>';
 		newPost += '<td class="author">' + post.author + '</td>'
@@ -22,41 +22,37 @@ $(document).ready(function() {
 		newPost += '<td class="message">' + post.message + '</td>'
 		newPost += '</tr>'
 		$('.posts tbody').append(newPost);
-    }
-    
-    var createForm = $('#create-post form');
-    createForm.constraints('/posts/constraints');
-    createForm.validate(); // Init validator
-    
-    createForm.submit(function(e) {
-    	// Perform validation checks before posting
-    	var validator = createForm.validate();
-    	if (validator.form()) {
-	    	// Send post to server
-			$.ajax('/posts', {  
-				type: 'POST',
-				data: createForm.serialize(),
-				success: function(data) {
-					$('.status p').text(data.message);
-					
-					if (data.success) {
-						appendPost(data.post);
-						$('.posts tbody tr:last').hide();
-						$('.posts tbody tr:last').fadeIn();
-						
-						// Clear creation form
-						$(':input', '#create-post')
-							.not(':button, :submit, :reset, :hidden')
-							.removeAttr('checked')
-							.removeAttr('selected')
-								.val('');
-						
-						validator.resetForm();
-					}
-				}
-			});  
-    	}
+	}
+
+	var createForm = $('#create-post form');
+	createForm.constraints('/posts/constraints');
+	createForm.validate(); // Init validator
+
+	createForm.submit(function(e) {
+		// Perform validation checks before posting
+		var validator = createForm.validate();
+		if (validator.form()) {
+			// Send post to server
+			$.ajax('/posts', {
+			    type : 'POST',
+			    data : createForm.serialize(),
+			    success : function(data) {
+				    $('#status').text(data.message);
+
+				    if (data.success) {
+					    appendPost(data.post);
+					    $('.posts tbody tr:last').hide();
+					    $('.posts tbody tr:last').fadeIn();
+
+					    // Clear creation form
+					    $(':input', '#create-post').not(':button, :submit, :reset, :hidden').removeAttr('checked').removeAttr('selected').val('');
+
+					    validator.resetForm();
+				    }
+			    }
+			});
+		}
 		return false;
 	});
-    
+
 });
