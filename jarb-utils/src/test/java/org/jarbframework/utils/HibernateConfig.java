7bcf4b5055027835fb20c3bc3373daa6c858565c
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2014-07-08 15:28:01 +0200

    Added hibernate utils and spring factory bean test

---
+++ jarb-utils/src/test/java/org/jarbframework/utils/HibernateConfig.java
@@ -0,0 +1,49 @@
+/*
+ * (C) 2013 42 bv (www.42.nl). All rights reserved.
+ */
+package org.jarbframework.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.jarbframework.utils.orm.hibernate.ConventionNamingStrategy;
+import org.jarbframework.utils.orm.hibernate.ImprovedHsqlDialect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+/**
+ * Simple configuration with Hibernate.
+ * 
+ * @author Jeroen van Schagen
+ * @since Jul 8, 2014
+ */
+@Configuration
+@Import(DataSourceConfig.class)
+public class HibernateConfig {
+    
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactoryBean.setPackagesToScan("org.jarbframework.utils.domain");
+        
+        Map<String, Object> jpaProperties = new HashMap<String, Object>();
+        jpaProperties.put("hibernate.ejb.naming_strategy", ConventionNamingStrategy.class.getName());
+        jpaProperties.put("hibernate.dialect", ImprovedHsqlDialect.class.getName());
+        jpaProperties.put("hibernate.hbm2ddl.auto", "validate");
+        
+        entityManagerFactoryBean.setJpaPropertyMap(jpaProperties);
+        return entityManagerFactoryBean;
+    }
+
+}
