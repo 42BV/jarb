be42aa03c48faa097a139f696482eaa9bc4baeb7
Bas de Vos <bas.de.vos@42.nl>
2019-11-21 17:01:14 +0100

    Added overloaded method on JdbcUtils for doWithConnection and optimized implementation.

--- src/main/java/nl/_42/jarb/utils/jdbc/JdbcUtils.java
+++ src/main/java/nl/_42/jarb/utils/jdbc/JdbcUtils.java
@@ -23,25 +23,41 @@ public final class JdbcUtils {
     /**
      * Open a data source connection and provide callback functionality on that connection.
      * After the callback has been invoked, the newly created connection will be closed.
+     * The connection from the datasource will not explicitly be committed. (thus only works for read operations or when
+     * datasource autocommit == true)
+     *
+     * @param <T> type of object returned by callback
+     * @param dataSource - the data source that we should use
+     * @param callback - the callback functionality being invoked
+     * @return result of the callback, if any
+     */
+    public static <T> T doWithConnection(DataSource dataSource, Callback<T> callback) {
+        return doWithConnection(dataSource, callback, false);
+    }
+
+    /**
+     * Open a data source connection and provide callback functionality on that connection.
+     * After the callback has been invoked, the newly created connection will be closed.
      * 
      * @param <T> type of object returned by callback
-     * @param dataSource the data source that we should use
-     * @param callback the callback functionality being invoked
+     * @param dataSource - the data source that we should use
+     * @param commit - if datasource autocommit == false and this arg is true, the connection is explicitly committed after callback
+     * @param callback - the callback functionality being invoked
      * @return result of the callback, if any
      */
     public static <T> T doWithConnection(DataSource dataSource, Callback<T> callback, boolean commit) {
         Connection connection = null;
-        boolean autoCommit = true;
+        boolean shouldCommit = false;
         try {
             connection = dataSource.getConnection();
-            autoCommit = connection.getAutoCommit();
+            shouldCommit = !connection.getAutoCommit() && commit;
             T result = callback.doWork(connection);
-            if (!autoCommit && commit) {
+            if (shouldCommit) {
                 connection.commit();
             }
             return result;
         } catch (SQLException e) {
-            if (!autoCommit && commit) {
+            if (shouldCommit) {
                 rollback(connection);
             }
             throw new RuntimeException(e);
